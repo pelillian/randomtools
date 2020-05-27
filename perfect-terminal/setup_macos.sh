271a219f5f650cfb77e7abc75c778b042c96f369
pelillian <plillian@usc.edu>
2020-05-27 12:51:46 -0700

    Fix key repeat

--- perfect-terminal/setup_macos.sh
+++ perfect-terminal/setup_macos.sh
@@ -5,4 +5,5 @@ source setup_common.sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "set completion-ignore-case On" >> ~/.inputrc
+defaults write NSGlobalDomain KeyRepeat -int 1
 
