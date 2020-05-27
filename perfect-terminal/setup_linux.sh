af003a77577197370031eb16d67d198a3252c26f
pelillian <plillian@usc.edu>
2020-05-27 12:47:48 -0700

    Custom setup for macos

--- perfect-terminal/setup_linux.sh
+++ perfect-terminal/setup_linux.sh
@@ -1,29 +1,9 @@
 #!/usr/bin/env bash
 
-cd
-mv ~/.vim ~/.vim-old
-mv ~/.vimrc ~/.vimrc.old
-mv ~/.zshrc ~/.zshrc.old
-mv ~/.tmux.conf ~/.tmux.conf.old
-mv ~/.inputrc ~/.inputrc.old
-ln -s ~/Git/randomtools/perfect-terminal/.vim ~/.vim
-ln -s ~/Git/randomtools/perfect-terminal/.vimrc ~/.vimrc
-ln -s ~/Git/randomtools/perfect-terminal/.zshrc ~/.zshrc
-ln -s ~/Git/randomtools/perfect-terminal/.tmux.conf ~/.tmux.conf
-ln -s ~/Git/randomtools/perfect-terminal/.inputrc ~/.inputrc
-
-echo 'source ~/.vimrc' >> ~/.ideavimrc
-
-cd ~/Git/randomtools/
-git config submodule.recurse true
-git submodule update --init
+source setup_common.sh
 
 # Remove trash can from desktop
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
-
-# Add our custom bashrc append
-cat ~/Git/randomtools/perfect-terminal/append.bashrc >> ~/.bashrc
-. ~/.bashrc
 
 # Perfect GNOME dock
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
---
+++ perfect-terminal/setup_macos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+source setup_common.sh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "set completion-ignore-case On" >> ~/.inputrc
+
