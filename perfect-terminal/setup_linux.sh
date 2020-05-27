158b5d2529436457c047b8e1c41efa591ca2502c
pelillian <plillian@usc.edu>
2020-05-27 12:00:08 -0700

    Remove zsh install

--- perfect-terminal/setup_linux.sh
+++ perfect-terminal/setup_linux.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-#sudo apt-get update
-#sudo apt-get install zsh -y
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
-
 cd
 mv ~/.vim ~/.vim-old
 mv ~/.vimrc ~/.vimrc.old
