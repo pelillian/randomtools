b2c65dc2a5a6d0c926ccacd43fb2f3e2a45d5878
pelillian <plillian@usc.edu>
2020-06-12 20:21:30 -0700

    Fix mac python shell

--- perfect-terminal/setup_macos.sh
+++ perfect-terminal/setup_macos.sh
@@ -5,5 +5,8 @@ source setup_common.sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "set completion-ignore-case On" >> ~/.inputrc
+echo "set editing-mode vi" >> ~/.editrc
+echo "bind "^R" em-inc-search-prev" >> ~/.editrc
+echo "set completion-ignore-case On" >> ~/.editrc
 defaults write NSGlobalDomain KeyRepeat -int 1
 
